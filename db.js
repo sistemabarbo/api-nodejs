ba150faed3544f9bfcaa01fa5f84696ea0faa59e
Barbosa <132204097+sistemabarbo@users.noreply.github.com>
2024-08-12 09:10:04 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
-import mysql from "mysql";
+/*import mysql from "mysql";
 
 export const db = mysql.createConnection({
     host: "b9lsqlxrc1wrcggnqosi-mysql.services.clever-cloud.com",
     user: "ugcnyroeqou4hr6n",
     password: "fmIducXVC9LOVxi6KgPB",
     database: "b9lsqlxrc1wrcggnqosi",
-});
+});*/
